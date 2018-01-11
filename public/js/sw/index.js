a216c197bf0eab985d008047496891f084ab06fc
swaption2009 <swaption2009@gmail.com>
2018-01-11 12:57:45 -0800

    replace old cache with new one dynamically

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,13 @@
+var staticCacheName = 'wittr-static-v2';
+var Promise = require('es6-promise').Promise;
+var cache;
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v2').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -17,10 +21,19 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    // TODO: remove the old cache
-      caches.delete('wittr-static-v1')
-  );
+    event.waitUntil(
+        // TODO: remove the old cache
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('wittr-')
+                        && cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return cache.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
