3ac35cd4d61c14906c1d7dc263b72e9836edae52
swaption2009 <swaption2009@gmail.com>
2018-01-11 14:13:50 -0800

    Notify User when there is new SW update

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,35 +1,31 @@
 var staticCacheName = 'wittr-static-v2';
-var Promise = require('es6-promise').Promise;
-var cache;
+
+// change in SW to trigger update-notify
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
-    // Ensure at least $primary-color changes
-    // TODO: change cache name to 'wittr-static-v2'
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'js/main.js',
+                'css/main.css',
+                'imgs/icon.png',
+                'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('activate', function(event) {
     event.waitUntil(
-        // TODO: remove the old cache
-        caches.keys().then(function (cacheNames) {
+        caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('wittr-')
-                        && cacheName != staticCacheName;
-                }).map(function (cacheName) {
-                    return cache.delete(cacheName);
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('wittr-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
                 })
             );
         })
@@ -37,9 +33,9 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
