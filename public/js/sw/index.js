b74caf8929ad8bf4b90a0ef257a8300f4ffb9211
swaption2009 <swaption2009@gmail.com>
2018-01-10 18:38:49 -0800

    hijack jpg images and replace with gif image

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,15 @@
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-      new Response("<h1 class='a-winner-is-me'>This is a class</h1>", {
-          headers: {'Content-Type': 'text/html'}
-      })
-  );
+    // return selective request hijacker, jpg files
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+
+  // return HTML static page
+  // event.respondWith(
+  //     new Response("<h1 class='a-winner-is-me'>This is a class</h1>", {
+  //         headers: {'Content-Type': 'text/html'}
+  //     })
+  // );
 });
